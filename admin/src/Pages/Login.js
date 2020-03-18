8db1d90c842ff4de36607f4aa396681fb8456de0
tianwei <464231692@qq.com>
2020-03-17 22:51:56 -0400

    login

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,11 +1,46 @@
 import React , {useState} from 'react';
 import 'antd/dist/antd.css';
+import '../static/css/Login.css';
 import { Card, Input, Icon,Button ,Spin } from 'antd';
+import { UserAddOutlined, CompassOutlined } from '@ant-design/icons';
 
 function Login(){
+
+    const [userName , setUserName] = useState('')
+    const [password , setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = ()=>{
+        setIsLoading(true)
+        setTimeout(()=>{
+            setIsLoading(false)
+        },1000)
+    }
+
     return (
-        <div>
-           login page
+        <div className="login-div">
+
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="My Blog  System" bordered={true} style={{ width: 400 }} >
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<UserAddOutlined />}
+                        onChange={(e)=>{setUserName(e.target.value)}}
+                    /> 
+                    <br/><br/>
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="Enter your password"
+                        prefix={<CompassOutlined />}
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                    />     
+                    <br/><br/>
+                    <Button type="primary" size="large" block onClick={checkLogin} > Login in </Button>
+                </Card>
+            </Spin>
         </div>
     )
 }
